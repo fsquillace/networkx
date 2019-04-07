b947ffa8cb1f8a032b430f067e916aee2e47e7a5
Filippo Squillace <squillace.filippo@gmail.com>
2019-04-07 20:49:30 +1000

    Add function `floyd_warshall_multi_predecessors_and_distance`

--- networkx/algorithms/shortest_paths/dense.py
+++ networkx/algorithms/shortest_paths/dense.py
@@ -14,7 +14,9 @@ import networkx as nx
 
 __all__ = ['floyd_warshall',
            'floyd_warshall_predecessor_and_distance',
+           'floyd_warshall_multi_predecessors_and_distance',
            'reconstruct_path',
+           'reconstruct_multi_paths',
            'floyd_warshall_numpy']
 
 
@@ -64,6 +66,37 @@ def floyd_warshall_numpy(G, nodelist=None, weight='weight'):
     return A
 
 
+def _floyd_warshall_predecessor_and_distance(G, multi_predecessors=False, weight='weight'):
+    from collections import defaultdict
+    # dictionary-of-dictionaries representation for dist and pred
+    # use some defaultdict magick here
+    # for dist the default is the floating point inf value
+    pred = defaultdict(lambda: defaultdict(set))
+    dist = defaultdict(lambda: defaultdict(lambda: float('inf')))
+    for u in G:
+        dist[u][u] = 0
+    # initialize path distance dictionary to be the adjacency matrix
+    # also set the distance to self to 0 (zero diagonal)
+    undirected = not G.is_directed()
+    for u, v, d in G.edges(data=True):
+        e_weight = d.get(weight, 1.0)
+        dist[u][v] = min(e_weight, dist[u][v])
+        pred[u][v] = {u} if multi_predecessors else u
+        if undirected:
+            dist[v][u] = min(e_weight, dist[v][u])
+            pred[v][u] = {v} if multi_predecessors else v
+    for w in G:
+        for u in G:
+            for v in G:
+                if dist[u][v] > dist[u][w] + dist[w][v]:
+                    dist[u][v] = dist[u][w] + dist[w][v]
+                    pred[u][v] = set()
+                    pred[u][v].update(pred[w][v])
+                elif multi_predecessors and dist[u][v] == dist[u][w] + dist[w][v]:
+                    pred[u][v].update(pred[w][v])
+    return dict(pred), dict(dist)
+
+
 def floyd_warshall_predecessor_and_distance(G, weight='weight'):
     """Find all-pairs shortest path lengths using Floyd's algorithm.
 
@@ -101,34 +134,59 @@ def floyd_warshall_predecessor_and_distance(G, weight='weight'):
     --------
     floyd_warshall
     floyd_warshall_numpy
+    floyd_warshall_multi_predecessors_and_distance
     all_pairs_shortest_path
     all_pairs_shortest_path_length
     """
-    from collections import defaultdict
-    # dictionary-of-dictionaries representation for dist and pred
-    # use some defaultdict magick here
-    # for dist the default is the floating point inf value
-    dist = defaultdict(lambda: defaultdict(lambda: float('inf')))
-    for u in G:
-        dist[u][u] = 0
-    pred = defaultdict(dict)
-    # initialize path distance dictionary to be the adjacency matrix
-    # also set the distance to self to 0 (zero diagonal)
-    undirected = not G.is_directed()
-    for u, v, d in G.edges(data=True):
-        e_weight = d.get(weight, 1.0)
-        dist[u][v] = min(e_weight, dist[u][v])
-        pred[u][v] = u
-        if undirected:
-            dist[v][u] = min(e_weight, dist[v][u])
-            pred[v][u] = v
-    for w in G:
-        for u in G:
-            for v in G:
-                if dist[u][v] > dist[u][w] + dist[w][v]:
-                    dist[u][v] = dist[u][w] + dist[w][v]
-                    pred[u][v] = pred[w][v]
-    return dict(pred), dict(dist)
+    return _floyd_warshall_predecessor_and_distance(
+        G, multi_predecessors=False, weight=weight
+    )
+
+
+def floyd_warshall_multi_predecessors_and_distance(G, weight='weight'):
+    """Find multiple all-pairs shortest paths using Floyd's algorithm.
+
+    Parameters
+    ----------
+    G : NetworkX graph
+
+    weight: string, optional (default= 'weight')
+       Edge data key corresponding to the edge weight.
+
+    Returns
+    -------
+    predecessors, distance : dictionaries
+       Dictionaries, keyed by source and target, of predecessors and distances
+       in the shortest paths.
+
+    Examples
+    --------
+    >>> G = nx.DiGraph()
+    >>> G.add_weighted_edges_from([('s', 'u', 10), ('s', 'x', 5),
+    ...     ('u', 'v', 1), ('u', 'x', 2), ('v', 'y', 1), ('x', 'u', 3),
+    ...     ('x', 'v', 4), ('x', 'y', 2), ('y', 's', 7), ('y', 'v', 6)])
+    >>> predecessors, _ = nx.floyd_warshall_multi_predecessors_and_distance(G)
+    >>> print(nx.reconstruct_multi_paths('s', 'v', predecessors))
+    [['s', 'x', 'u', 'v'], ['s', 'x', 'v']]
+
+    Notes
+    ------
+    Floyd's algorithm is appropriate for finding shortest paths
+    in dense graphs or graphs with negative weights when Dijkstra's algorithm
+    fails.  This algorithm can still fail if there are negative cycles.
+    It has running time $O(n^3)$ with running space of $O(n^3)$.
+
+    See Also
+    --------
+    floyd_warshall
+    floyd_warshall_numpy
+    floyd_warshall_predecessor_and_distance
+    all_pairs_shortest_path
+    all_pairs_shortest_path_length
+    """
+    return _floyd_warshall_predecessor_and_distance(
+        G, multi_predecessors=True, weight=weight
+    )
 
 
 def reconstruct_path(source, target, predecessors):
@@ -172,6 +230,63 @@ def reconstruct_path(source, target, predecessors):
         curr = prev[curr]
         path.append(curr)
     return list(reversed(path))
+
+
+def reconstruct_multi_paths(source, target, predecessors):
+    """Reconstruct the multiple paths from source to target using the predecessors
+    dict as returned by floyd_warshall_multi_predecessors_and_distance
+
+    Parameters
+    ----------
+    source : node
+       Starting node for path
+
+    target : node
+       Ending node for path
+
+    predecessors: dictionary
+       Dictionary, keyed by source and target, of predecessors in the
+       shortest paths, as returned by floyd_warshall_multi_predecessors_and_distance
+
+    Returns
+    -------
+    path : list
+       A list of paths which represents the shortest paths
+       from source to target. A path is a list of nodes.
+
+       If source and target are the same, an empty list is returned
+
+    Notes
+    ------
+    This function is meant to give more applicability to the
+    floyd_warshall_multi_predecessors_and_distance function
+
+    See Also
+    --------
+    floyd_warshall_multi_predecessors_and_distance
+    """
+    if source == target:
+        return []
+    paths = [[target]]
+    queue = [target]
+    while queue:
+        curr = queue.pop(0)
+
+        new_paths = []
+        for path in paths:
+            if path[0] == curr:
+                for neighbor in predecessors[source][curr]:
+                    new_path = list(path)
+                    new_path.insert(0, neighbor)
+                    new_paths.append(new_path)
+            else:
+                new_paths.append(path)
+        paths = new_paths
+
+        for neighbor in predecessors[source][curr]:
+            if neighbor != source:
+                queue.append(neighbor)
+    return paths
 
 
 def floyd_warshall(G, weight='weight'):
